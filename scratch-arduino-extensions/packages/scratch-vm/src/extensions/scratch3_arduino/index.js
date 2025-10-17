9dfb84933f2004481a002959c11ca0c8d94637b9
Davide <davideneri18@gmail.com>
2025-10-17 09:58:23 +0200

    feat(scratch): Add scaffolding ArduinoScratch extension (#1)
    * Add initial configuration and tasks for Scratch Arduino integration
    
    * Remove redundant cleanup command from scratch:init task and comment out formatMessage import in Arduino extension
    
    * Refactor task names in Taskfile.yaml for consistency and update Scratch3Arduino class to include example block and clean up imports
    
    * Update Scratch3Arduino block opcode and remove commented-out code in patch-gui.js

---
+++ scratch-arduino-extensions/packages/scratch-vm/src/extensions/scratch3_arduino/index.js
@@ -0,0 +1,46 @@
+// Option 1: Relative path to cousin folder
+// const formatMessage = require('../../../../../../scratch-editor/node_modules/format-message');
+const BlockType = require('../../../../../../scratch-editor/packages/scratch-vm/src/extension-support/block-type');
+
+/**
+ * Url of icon to be displayed at the left edge of each extension block.
+ * @type {string}
+ */
+// eslint-disable-next-line max-len
+const iconURI = ''; 
+
+/**
+ * Url of icon to be displayed in the toolbox menu for the extension category.
+ * @type {string}
+ */
+// eslint-disable-next-line max-len
+const menuIconURI =  ''
+
+class Scratch3Arduino {
+    constructor (runtime) {
+        this.runtime = runtime;
+   }
+};
+
+Scratch3Arduino.prototype.getInfo = function () {
+        return {
+            id: 'arduino',
+            name: "Arduino",
+            menuIconURI: menuIconURI,
+            blockIconURI: iconURI,
+            blocks: [ 
+                {
+                    opcode: 'noop',
+                    blockType: BlockType.COMMAND,
+                    text: 'do nothing',
+                    func: 'noop'
+                },
+            ],
+        
+        };
+}
+
+Scratch3Arduino.prototype.noop = function () {
+};
+
+module.exports = Scratch3Arduino;
