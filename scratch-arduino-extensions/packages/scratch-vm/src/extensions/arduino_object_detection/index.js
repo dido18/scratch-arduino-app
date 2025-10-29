559da35de88e5332a8e1a78d4e8da6701f0b91ce
dido18 <davideneri18@gmail.com>
2025-10-30 00:06:25 +0100

    fix: update enableVideo method to capture and emit video frames in base64 format

--- scratch-arduino-extensions/packages/scratch-vm/src/extensions/arduino_object_detection/index.js
+++ scratch-arduino-extensions/packages/scratch-vm/src/extensions/arduino_object_detection/index.js
@@ -4,6 +4,7 @@ const ArgumentType = require(
   "../../../../../../scratch-editor/packages/scratch-vm/src/extension-support/argument-type",
 );
 const io = require("../socket.io.min.js");
+const Video = require('../../../../../../scratch-editor/packages/scratch-vm/src/io/video');
 
 /**
  * Url of icon to be displayed at the left edge of each extension block.
@@ -19,7 +20,7 @@ const iconURI = "";
 // eslint-disable-next-line max-len
 const menuIconURI = "";
 
-const wsServerURL = `${window.location.protocol}//${window.location.hostname}:7000`;
+const wsServerURL = `ws://192.168.1.39:7000`;
 
 class arduinoObjectDetection {
   constructor(runtime) {
@@ -58,19 +59,27 @@ arduinoObjectDetection.prototype.getInfo = function() {
   };
 };
 
-arduinoObjectDetection.prototype.enableVideo = async function(args) {
+arduinoObjectDetection.prototype.enableVideo = function(args) {
    this.runtime.ioDevices.video.enableVideo();
 
-    const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    if (this.runtime.ioDevices) {
+        // Get frame as canvas for base64 conversion
+        const canvas = this.runtime.ioDevices.video.getFrame({
+            format: Video.FORMAT_CANVAS,
+            dimensions: [320, 240]
+        });
 
-    for (let i = 0; i < 10; i++) {
-        if (this.runtime.ioDevices) {
-            const frame = this.runtime.ioDevices.video.getFrame({format: 'image-data'});
-            // Sleep for 1 second (1000ms) after each frame
-            console.log(`Processed frame ${i + 1}`, frame);
-            this.io.emit('detect_objects', {image: frame});
-            await sleep(1000);
+        if (canvas) {
+            // Convert canvas to base64 PNG (lossless) or JPEG (smaller file)
+            const base64Frame = canvas.toDataURL('image/png'); // PNG format
+            // Alternative: canvas.toDataURL('image/jpeg', 0.8); // JPEG format with 80% quality
+            this.io.emit('detect_objects', { image: base64Frame });
+            console.log(`Processed frame in base64`, base64Frame.substring(0, 100) + '...');
+        } else {
+            console.log("Failed to capture frame.");
         }
+    } else {
+        console.log("No ioDevices available.");
     }
 
 };
